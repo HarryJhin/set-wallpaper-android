aa19d4f39d814253aeb04125dafec90043f78872
jjh <joojinhyun00@gmail.com>
2022-05-19 10:26:17 +0900

    이미지 추가 및 버튼으로 설정

--- app/src/main/java/com/example/setwallpaper/MainActivity.kt
+++ app/src/main/java/com/example/setwallpaper/MainActivity.kt
@@ -17,13 +17,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.white_wallpaper) // 배경 화면으로 설정할 이미지를 Bitmap 타입으로 변경
+        val whiteWallpaper = BitmapFactory.decodeResource(resources, R.drawable.white_wallpaper) // 배경 화면으로 설정할 이미지를 Bitmap 타입으로 변경
+        val blackWallpaper = BitmapFactory.decodeResource(resources, R.drawable.black_wallpaper) // 배경 화면으로 설정할 이미지를 Bitmap 타입으로 변경
 
         val wallpaperManager = WallpaperManager.getInstance(this) // 배경 화면 변경을 위한 인스턴스
-        try {
-            wallpaperManager.setBitmap(bitmap) // 배경화면 설정
-        } catch (e: Exception) {
-            Log.e("MainActivity", "setBitmap -> e.message: " + e.message.toString())
+
+
+        binding.buttonLeft.setOnClickListener {
+            try {
+                wallpaperManager.setBitmap(whiteWallpaper) // 배경화면 설정
+            } catch (e: Exception) {
+                Log.e("MainActivity", "setBitmap -> e.message: " + e.message.toString())
+            }
+        }
+        binding.buttonRight.setOnClickListener {
+            try {
+                wallpaperManager.setBitmap(blackWallpaper) // 배경화면 설정
+            } catch (e: Exception) {
+                Log.e("MainActivity", "setBitmap -> e.message: " + e.message.toString())
+            }
         }
     }
 }
