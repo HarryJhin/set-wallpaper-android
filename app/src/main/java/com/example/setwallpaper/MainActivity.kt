e3a60ebb2b08e281d842e5ad04e5d4fa45073914
jjh <joojinhyun00@gmail.com>
2022-05-19 09:55:27 +0900

    wallpaper 변경

--- app/src/main/java/com/example/setwallpaper/MainActivity.kt
+++ app/src/main/java/com/example/setwallpaper/MainActivity.kt
@@ -1,16 +1,29 @@
 package com.example.setwallpaper
 
+import android.app.WallpaperManager
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.setwallpaper.databinding.ActivityMainBinding
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-    val binding by lazy {
+    private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.white_wallpaper) // 배경 화면으로 설정할 이미지를 Bitmap 타입으로 변경
+
+        val wallpaperManager = WallpaperManager.getInstance(this) // 배경 화면 변경을 위한 인스턴스
+        try {
+            wallpaperManager.setBitmap(bitmap) // 배경화면 설정
+        } catch (e: Exception) {
+            Log.e("MainActivity", "setBitmap -> e.message: " + e.message.toString())
+        }
     }
 }
